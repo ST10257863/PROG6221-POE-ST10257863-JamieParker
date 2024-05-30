51638d7da2a29843634d5b71ef8606f28dae9975
Jamie Parker <129764378+ST10257863@users.noreply.github.com>
2024-05-30 16:26:51 +0200

    Refactoring Ingredient

--- Classes/Ingredient.cs
+++ Classes/Ingredient.cs
@@ -1,34 +1,20 @@
 ﻿namespace PROG6221_POE_ST10257863_JamieParker.Classes
 {
-	internal class Ingredient
+
+	public class Ingredient
 	{
-		public Ingredient()
-		{
-		}
-
-		public Ingredient(string name, double amount, string measurment, double calories)
-		{
-			this.Name = name;
-			this.Amount = amount;
-			this.Measurment = measurment;
-			this.Calories = calories;
-		}
-
 		public string Name
 		{
 			get; set;
 		}
-
 		public double Amount
 		{
 			get; set;
 		}
-
-		public string Measurment
+		public string Measurement
 		{
 			get; set;
 		}
-
 		public double Calories
 		{
 			get; set;
@@ -36,7 +22,16 @@
 		public string FoodGroup
 		{
 			get; set;
-		} // New property for food group
+		}
 
+		public Ingredient(string name, double amount, string measurement, double calories, string foodGroup)
+		{
+			Name = name;
+			Amount = amount;
+			Measurement = measurement;
+			Calories = calories;
+			FoodGroup = foodGroup;
+		}
 	}
+
 }
